df6846d00a9a046a0f2f905c744575edd4d8df7a
Jash <jash113355@gmail.com>
2024-07-06 14:16:12 +0530

    demo purpose project commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Hello from the java.");
+        System.out.println("This is the java program run.");
+        System.out.println("This program run on :- " + new Date());   
+    }
+}
